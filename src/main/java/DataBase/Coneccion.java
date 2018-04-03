b07e89831c4d72f3c9ced802ebd14113f92a22b5
jgutierrez <gojorge1@gmail.com>
2018-04-03 13:00:39 -0600

    Cambios en la clase de conexion de la base de datos.

--- src/main/java/DataBase/Coneccion.java
+++ src/main/java/DataBase/Coneccion.java
@@ -36,7 +36,7 @@ public class Coneccion {
     public int Prueba(){
         int result = -1;
         Conectar();
-        if(con!=null) result = 1;
+        if(con!=null) result = 0;
         else result = 0;
         /*
         Statement s = con.createStatement();
